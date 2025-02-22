c044e098f646b637631dbc0e2ee595d82403aea9
rw-bsi <raphael+buildingsmart@xeneo.biz>
2025-02-22 16:19:23 +0000

    Store company email address pattern(s)

--- models.py
+++ models.py
@@ -227,6 +227,14 @@ class Company(TimestampedBaseModel):
         help_text="Legal name of the Company (optional)."
     )
 
+    email_address_pattern = models.CharField(
+        max_length=1024,
+        null=True,
+        blank=True,
+        unique=True,
+        help_text="Email address pattern(s) of the Company (optional)."
+    )
+
     class Meta:
 
         db_table = "ifc_company"
