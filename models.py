d5b31c787f2327f5c6aa6c35f63d618a09786641
Geert Hesselink <geert.hess@gmail.com>
2025-06-25 19:29:08 +0100

    small fixes

--- models.py
+++ models.py
@@ -907,7 +907,7 @@ class ValidationTask(TimestampedBaseModel, IdObfuscator):
         The type of an Validation Task.
         """
         SYNTAX              = 'SYNTAX', 'STEP Physical File Syntax'
-        SYNTAX_HEADER       = 'SYNTAX_HEADER', 'STEP Physical File Syntax (HEADER; section)'
+        SYNTAX_HEADER       = 'SYNTAX_HEADER', 'STEP Physical File Syntax (HEADER section)'
         SCHEMA              = 'SCHEMA', 'Schema (EXPRESS language)'
         MVD                 = 'MVD', 'Model View Definitions'
         BSDD                = 'BSDD', 'bSDD Compliance'
